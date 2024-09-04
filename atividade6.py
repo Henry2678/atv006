89eff2e28b0341677d4d607dd6867f2a17b84789
Henry2678 <soldadohuefl@gmail.com>
2024-09-04 15:44:06 -0300

    atividade6

---
+++ atividade6.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+valor = str(input("digite seu primeiro nome "))
+valor2 = str(input("digite seu sobrenome "))
+nome = valor + valor2
+print(f"seu nome é {nome}") 
